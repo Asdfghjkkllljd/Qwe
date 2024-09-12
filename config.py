5d26ade1606e74d5a6afefcefc810a4e30b0a28c
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-12 19:43:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-TOKEN = "7429518608:AAFMU2R_CyyLqPhWl2LJTWxTQVPU1DhSppk" # Токен бота
-CHANNEL_ID = -1002243436081 # ID канала со ставками
-ADMINS = [5846969551] # ID админов
-BET_URL = "t.me/send?start=IVpAqLJ8Pztg" # Ссылка на счет
-CRYPTOPAY_TOKEN = "251801:AAaOA42R6B4OjqU4ciwBt2KRjmAHfyPx6mY" # Тут CryptoPay токен
+TOKEN = "7397021024:AAGfIUsT73r4RjZEaXh9RmHpKvoV-DEfnb8" # Токен бота
+CHANNEL_ID = -1002487160823 # ID канала со ставками
+ADMINS = [7196884898] # ID админов
+BET_URL = "http://t.me/send?start=IVl0BR7oJ8QZ" # Ссылка на счет
+CRYPTOPAY_TOKEN = "268359:AAqHvDNmND80ZC7INyZLy4uQzd4HptM5XQ1" # Тут CryptoPay токен
 WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
-LOGS_ID = -1002226990800 # ID канала с оплатами
+LOGS_ID = -1002375578630 # ID канала с оплатами
 
-RULES_LINK = "https://t.me/+k42zVBd8Iz41ZDEy" # Ссылка на канал с правилами
-NEWS_LINK = "https://t.me/+YXO5t3AYIRgxZTdi" # Ссылка на новостной канал
-OWNER_LINK = "https://t.me/CEKC_HA_OCTPOBAX" # Ссылка на владельца/поддержку
-PEREHOD_LINK = "@extremecasinoo" # Ссылка на переходник
-BETS_LINK = "https://t.me/extremecasinoo" # Ссылка на канал ставок
-CASINO_NAME = "EXTREME CASINO" # Название казино
+RULES_LINK = "https://t.me/+zluKvP4xg4dlZTg6" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/+hXtU3Vk7Khc0ZWVi" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/mars55design" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/perehodcowel" # Ссылка на переходник
+BETS_LINK = "https://t.me/+3_m1LDdyWlRiNGIy" # Ссылка на канал ставок
+CASINO_NAME = "COWEL BET" # Название казино
